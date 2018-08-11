fd9904d245f5be8dd2438f3f26daa5ecedb1be0e
Rachel Hovell <r.hovell@live.com>
2018-08-11 16:13:33 +0100

    Fixed Initial app rendering issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-class App extends Component {
-  render() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-  }
-}
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
