450dbd1043a76d24757517d261908f40ff21d886
Rachel Hovell <r.hovell@live.com>
2018-08-12 17:53:12 +0100

    Added Create New Contact feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
@@ -13,19 +15,41 @@ class App extends Component {
   }
   removeContact = (contact) => {
     this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
+      contacts: state.contacts.filter(
+        c => c.id !== contact.id
+    )
     }))
     ContactsAPI.remove(contact)
   }
+  CreateContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
   render() {
     return (
-      <div>
+      <div className='app'>
+      <Route exact path="/" render={() => (
         <ListContacts
         onDeleteContact={this.removeContact}
-        contacts={this.state.contacts}/>
-      </div>
-    )
+        contacts={this.state.contacts}
+        />
+      )}/>
+      <Route path="/create" render={({ history }) => (
+        <CreateContact
+        onCreateContact={(contact) => {
+          this.CreateContact(contact)
+          history.push('/')
+        }}
+        />
+  )}/>
+  </div>
+)
   }
 }
+
+
 
 export default App;
